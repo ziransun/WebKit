3108ecd96d79d4e9cb09d193524559040caef92a
Jean-Yves Avenard <jya@apple.com>
2024-12-02 15:01:28 -0800

    Compilation failure error: member access into incomplete type 'const WebCore::RenderStyle' https://bugs.webkit.org/show_bug.cgi?id=283945 rdar://140817964
    Unreviewed unified build fix.
    
    Add missing header.
    
    * Source/WebCore/platform/graphics/coretext/FontCascadeCoreText.cpp:
    
    Canonical link: https://commits.webkit.org/287237@main

--- Source/WebCore/platform/graphics/coretext/FontCascadeCoreText.cpp
+++ Source/WebCore/platform/graphics/coretext/FontCascadeCoreText.cpp
@@ -30,6 +30,7 @@
 #include "GraphicsContext.h"
 #include "LayoutRect.h"
 #include "Logging.h"
+#include "RenderStyle.h"
 #include <pal/spi/cg/CoreGraphicsSPI.h>
 #include <wtf/MathExtras.h>
 #include <wtf/RuntimeApplicationChecks.h>
