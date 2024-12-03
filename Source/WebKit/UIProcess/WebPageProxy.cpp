ziransun/WebKit

commit be5caf9bf0c4db410091fd1dc0d655780f3b7b4e

Remove web process history item maps and begin using session state stored in the UI process https://bugs.webkit.org/show_bug.cgi?id=281844 rdar://138295365
Reviewed by Alex Christensen.

This patch removes the hash maps that store all history items in the web process, replacing them with the
session state stored in the UI process. Now, instead of the UI process using `BackForwardItemIdentifier`
to reference history items in the web process, the full history state is sent over IPC. This change will
make the implementation of navigation and session history with site isolation much simpler.

I needed to make the following changes to make this work correctly:
 - Compare history items using `BackForwardItemIdentifier` instead of their addresses in memory. Since
   history items are now constructed using state retrieved from the UI process, they can no longer be
   compared using their addresses. `BackForwardItemIdentifier` is unique per history item, and preserved
   when sent between processes.

 - Track back/forward state restored from another session in the UI process. Previously, when restoring
   session state, the state was sent to the web process to be stored in the history maps, and a boolean
   was set to indicate that the history item was restored.

 - Add state to `FrameState` that previously only existed on `HistoryItem`.

 - Update session state on `WebBackForwardListFrameItem` when its corresponding `HistoryItem` in the web
   process changes.

More details provided below.

* Source/WebCore/history/BackForwardCache.cpp:
(WebCore::BackForwardCache::remove):
* Source/WebCore/history/BackForwardCache.h:
Add a remove function that takes a `BackForwardItemIdentifier`, so the UI process can remove back/forward
cache state without a `HistoryItem`.

* Source/WebCore/history/HistoryItem.cpp:
(WebCore::HistoryItem::clearChildren):
Notify the UI process when a history item’s children have been cleared.

(WebCore::HistoryItem::setDocumentState):

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -1579,7 +1579,7 @@ RefPtr<API::Navigation> WebPageProxy::launchProcessForReload()
     auto publicSuffix = WebCore::PublicSuffixStore::singleton().publicSuffix(URL(backForwardList->currentItem()->url()));
 
     // We allow stale content when reloading a WebProcess that's been killed or crashed.
-    send(Messages::WebPage::GoToBackForwardItem({ navigation->navigationID(), backForwardList->currentItem()->itemID(), FrameLoadType::IndexedBackForward, ShouldTreatAsContinuingLoad::No, std::nullopt, m_lastNavigationWasAppInitiated, std::nullopt, publicSuffix, { } }));
+    send(Messages::WebPage::GoToBackForwardItem({ navigation->navigationID(), backForwardList->currentItem()->rootFrameState(), FrameLoadType::IndexedBackForward, ShouldTreatAsContinuingLoad::No, std::nullopt, m_lastNavigationWasAppInitiated, std::nullopt, publicSuffix, { } }));
 
     Ref legacyMainFrameProcess = m_legacyMainFrameProcess;
     legacyMainFrameProcess->startResponsivenessTimer();
@@ -2510,7 +2510,7 @@ RefPtr<API::Navigation> WebPageProxy::goToBackForwardItem(WebBackForwardListItem
     process->markProcessAsRecentlyUsed();
 
     auto publicSuffix = WebCore::PublicSuffixStore::singleton().publicSuffix(URL(item.url()));
-    process->send(Messages::WebPage::GoToBackForwardItem({ navigation->navigationID(), frameItem.rootFrame().identifier(), frameLoadType, ShouldTreatAsContinuingLoad::No, std::nullopt, m_lastNavigationWasAppInitiated, std::nullopt, WTFMove(publicSuffix), { } }), webPageIDInProcess(process));
+    process->send(Messages::WebPage::GoToBackForwardItem({ navigation->navigationID(), frameItem.rootFrame().frameState(), frameLoadType, ShouldTreatAsContinuingLoad::No, std::nullopt, m_lastNavigationWasAppInitiated, std::nullopt, WTFMove(publicSuffix), { } }), webPageIDInProcess(process));
     process->startResponsivenessTimer();
 
     return RefPtr<API::Navigation> { WTFMove(navigation) };
@@ -5206,7 +5206,7 @@ RefPtr<API::Navigation> WebPageProxy::restoreFromSessionState(SessionState sessi
         backForwardList->restoreFromState(WTFMove(sessionState.backForwardListState));
         // If the process is not launched yet, the session will be restored when sending the WebPageCreationParameters;
         if (hasRunningProcess())
-            send(Messages::WebPage::RestoreSession(backForwardList->itemStates()));
+            backForwardList->setItemsAsRestoredFromSession();
 
         auto transaction = internals().pageLoadState.transaction();
         internals().pageLoadState.setCanGoBack(transaction, backForwardList->backItem());
@@ -9266,7 +9266,7 @@ void WebPageProxy::backForwardAddItemShared(IPC::Connection& connection, FrameId
 
     if (RefPtr targetFrame = WebFrameProxy::webFrame(targetFrameID)) {
         if (RefPtr pendingChildBackForwardItem = targetFrame->takePendingChildBackForwardItem())
-            return pendingChildBackForwardItem->addChild(WTFMove(rootFrameState));
+            return pendingChildBackForwardItem->setChild(WTFMove(rootFrameState));
     }
 
     RefPtr provisionalPage = m_provisionalPage;
@@ -9285,7 +9285,7 @@ void WebPageProxy::backForwardAddItemShared(IPC::Connection& connection, FrameId
 void WebPageProxy::backForwardSetChildItem(BackForwardItemIdentifier identifier, Ref<FrameState>&& frameState)
 {
     if (RefPtr frameItem = WebBackForwardListFrameItem::itemForID(identifier))
-        frameItem->addChild(WTFMove(frameState));
+        frameItem->setChild(WTFMove(frameState));
 }
 
 void WebPageProxy::backForwardGoToItem(const BackForwardItemIdentifier& itemID, CompletionHandler<void(const WebBackForwardListCounts&)>&& completionHandler)
@@ -9340,15 +9340,15 @@ void WebPageProxy::backForwardClearProvisionalItem(IPC::Connection& connection, 
         protectedBackForwardList()->clearProvisionalItem(*frameItem);
 }
 
-void WebPageProxy::backForwardItemAtIndex(int32_t index, FrameIdentifier frameID, CompletionHandler<void(std::optional<BackForwardItemIdentifier>&&)>&& completionHandler)
+void WebPageProxy::backForwardItemAtIndex(int32_t index, FrameIdentifier frameID, CompletionHandler<void(RefPtr<FrameState>&&)>&& completionHandler)
 {
     // FIXME: This should verify that the web process requesting the item hosts the specified frame.
     if (RefPtr item = protectedBackForwardList()->itemAtIndex(index)) {
         if (auto* childFrameItem = item->rootFrameItem().childItemForFrameID(frameID))
-            return completionHandler(childFrameItem->identifier());
-        completionHandler(item->itemID());
+            return completionHandler(&childFrameItem->frameState());
+        completionHandler(&item->rootFrameState());
     } else
-        completionHandler(std::nullopt);
+        completionHandler(nullptr);
 }
 
 void WebPageProxy::backForwardListCounts(CompletionHandler<void(WebBackForwardListCounts&&)>&& completionHandler)
@@ -11055,6 +11055,8 @@ WebPageCreationParameters WebPageProxy::creationParameters(WebProcessProxy& proc
         userContentController = userContentControllerFromWebsitePolicies.releaseNonNull();
     process.addWebUserContentControllerProxy(userContentController);
 
+    protectedBackForwardList()->setItemsAsRestoredFromSession();
+
     RefPtr pageClient = this->pageClient();
 
     WebPageCreationParameters parameters {
@@ -11098,8 +11100,6 @@ WebPageCreationParameters WebPageProxy::creationParameters(WebProcessProxy& proc
     parameters.pageLength = m_pageLength;
     parameters.gapBetweenPages = m_gapBetweenPages;
     parameters.userAgent = userAgent();
-    parameters.itemStatesWereRestoredByAPIRequest = m_sessionStateWasRestoredByAPIRequest;
-    parameters.itemStates = protectedBackForwardList()->itemStates();
     parameters.canRunBeforeUnloadConfirmPanel = m_uiClient->canRunBeforeUnloadConfirmPanel();
     parameters.canRunModal = m_canRunModal;
     parameters.deviceScaleFactor = deviceScaleFactor();

Source/WebCore/history/BackForwardCache.cpp
@@ -615,18 +615,23 @@ CachedPage* BackForwardCache::get(HistoryItem& item, Page* page)
     });
 }
 
-void BackForwardCache::remove(HistoryItem& item)
+void BackForwardCache::remove(BackForwardItemIdentifier itemID)
 {
     // Safely ignore attempts to remove items not in the cache.
-    auto it = m_cachedPageMap.find(item.identifier());
+    auto it = m_cachedPageMap.find(itemID);
     if (it == m_cachedPageMap.end() || std::holds_alternative<PruningReason>(it->value))
         return;
 
-    m_items.remove(item.identifier());
+    m_items.remove(itemID);
     m_cachedPageMap.remove(it);
-    item.notifyChanged();
 
-    RELEASE_LOG(BackForwardCache, "BackForwardCache::remove item: %s, size: %u / %u", item.identifier().toString().utf8().data(), pageCount(), maxSize());
+    RELEASE_LOG(BackForwardCache, "BackForwardCache::remove item: %s, size: %u / %u", itemID.toString().utf8().data(), pageCount(), maxSize());
+}
+
+void BackForwardCache::remove(HistoryItem& item)
+{
+    remove(item.identifier());
+    item.notifyChanged();
 }
 
 void BackForwardCache::prune(PruningReason pruningReason)

Source/WebCore/history/BackForwardCache.h
@@ -56,6 +56,7 @@ public:
 
     WEBCORE_EXPORT std::unique_ptr<CachedPage> suspendPage(Page&);
     WEBCORE_EXPORT bool addIfCacheable(HistoryItem&, Page*); // Prunes if maxSize() is exceeded.
+    WEBCORE_EXPORT void remove(BackForwardItemIdentifier);
     WEBCORE_EXPORT void remove(HistoryItem&);
     CachedPage* get(HistoryItem&, Page*);
     std::unique_ptr<CachedPage> take(HistoryItem&, Page*);

Source/WebCore/history/HistoryItem.cpp
@@ -65,10 +65,7 @@ HistoryItem::HistoryItem(Client& client, const String& urlString, const String& 
 {
 }
 
-HistoryItem::~HistoryItem()
-{
-    ASSERT(!BackForwardCache::singleton().isInBackForwardCache(m_identifier));
-}
+HistoryItem::~HistoryItem() = default;
 
 HistoryItem::HistoryItem(const HistoryItem& item)
     : RefCounted<HistoryItem>()
@@ -264,6 +261,7 @@ void HistoryItem::setPageScaleFactor(float scaleFactor)
 void HistoryItem::setDocumentState(const Vector<AtomString>& state)
 {
     m_documentState = state;
+    notifyChanged();
 }
 
 const Vector<AtomString>& HistoryItem::documentState() const
@@ -353,6 +351,7 @@ const Vector<Ref<HistoryItem>>& HistoryItem::children() const
 void HistoryItem::clearChildren()
 {
     m_children.clear();
+    notifyChanged();
 }
 
 // We do same-document navigation if going to a different item and if either of the following is true:
@@ -361,7 +360,7 @@ void HistoryItem::clearChildren()
 bool HistoryItem::shouldDoSameDocumentNavigationTo(HistoryItem& otherItem) const
 {
     // The following logic must be kept in sync with WebKit::WebBackForwardListItem::itemIsInSameDocument().
-    if (this == &otherItem)
+    if (m_identifier == otherItem.identifier())
         return false;
 
     if (stateObject() || otherItem.stateObject())

Source/WebCore/loader/FrameLoader.cpp
@@ -2320,7 +2320,7 @@ void FrameLoader::commitProvisionalLoad()
         document->checkedEditor()->confirmOrCancelCompositionAndNotifyClient();
     }
 
-    if (!frame->tree().parent() && frame->history().currentItem() && frame->history().currentItem() != frame->history().provisionalItem()) {
+    if (!frame->tree().parent() && frame->history().currentItem() && (!frame->history().provisionalItem() || frame->history().currentItem()->identifier() != frame->history().provisionalItem()->identifier())) {
         // Check to see if we need to cache the page we are navigating away from into the back/forward cache.
         // We are doing this here because we know for sure that a new page is about to be loaded.
         BackForwardCache::singleton().addIfCacheable(*frame->history().protectedCurrentItem(), frame->protectedPage().get());

Source/WebCore/loader/HistoryController.cpp
@@ -256,7 +256,7 @@ void HistoryController::restoreDocumentState()
     RefPtr currentItem = m_currentItem;
     if (!currentItem)
         return;
-    if (frame->loader().requestedHistoryItem() != currentItem.get())
+    if (!frame->loader().requestedHistoryItem() || (frame->loader().requestedHistoryItem()->identifier() != currentItem->identifier()))
         return;
     RefPtr documentLoader = frame->loader().documentLoader();
     if (documentLoader->isClientRedirect())
@@ -855,7 +855,7 @@ bool HistoryController::itemsAreClones(HistoryItem& item1, HistoryItem* item2) c
     // new document and should not consider them clones.
     // (See http://webkit.org/b/35532 for details.)
     return item2
-        && &item1 != item2
+        && item1.identifier() != item2->identifier()
         && item1.itemSequenceNumber() == item2->itemSequenceNumber();
 }
 
@@ -968,6 +968,7 @@ void HistoryController::replaceState(RefPtr<SerializedScriptValue>&& stateObject
     currentItem->setStateObject(WTFMove(stateObject));
     currentItem->setFormData(nullptr);
     currentItem->setFormContentType(String());
+    currentItem->notifyChanged();
 
     RefPtr frame = dynamicDowncast<LocalFrame>(m_frame.ptr());
     if (!frame)

Source/WebCore/loader/NavigationScheduler.cpp
@@ -299,7 +299,7 @@ public:
 
         UserGestureIndicator gestureIndicator(userGestureToForward());
 
-        if (page->checkedBackForward()->currentItem() == m_historyItem.ptr()) {
+        if (page->checkedBackForward()->currentItem() && page->checkedBackForward()->currentItem()->identifier() == m_historyItem->identifier()) {
             // Special case for go(0) from a frame -> reload only the frame
             // To follow Firefox and IE's behavior, history reload can only navigate the self frame.
             if (RefPtr localFrame = dynamicDowncast<LocalFrame>(frame))
@@ -352,7 +352,7 @@ public:
 
         UserGestureIndicator gestureIndicator(userGestureToForward());
 
-        if (page->backForward().currentItem() == historyItem.ptr()) {
+        if (page->backForward().currentItem() && page->backForward().currentItem()->identifier() == historyItem->identifier()) {
             if (RefPtr localFrame = dynamicDowncast<LocalFrame>(frame))
                 localFrame->protectedLoader()->changeLocation(localFrame->document()->url(), selfTargetFrameName(), 0, ReferrerPolicy::EmptyString, shouldOpenExternalURLs(), std::nullopt, nullAtom(), std::nullopt, NavigationHistoryBehavior::Reload);
             return;

Source/WebCore/page/Page.cpp
@@ -543,7 +543,7 @@ void Page::firstTimeInitialization()
     });
 }
 
-void Page::clearPreviousItemFromAllPages(HistoryItem* item)
+void Page::clearPreviousItemFromAllPages(BackForwardItemIdentifier itemID)
 {
     for (auto& page : allPages()) {
         RefPtr localMainFrame = dynamicDowncast<LocalFrame>(page->mainFrame());
@@ -551,7 +551,7 @@ void Page::clearPreviousItemFromAllPages(HistoryItem* item)
             return;
 
         CheckedRef controller = localMainFrame->history();
-        if (item == controller->previousItem()) {
+        if (controller->previousItem() && controller->previousItem()->identifier() == itemID) {
             controller->clearPreviousItem();
             return;
         }

commit 7525d1bcf3a15c4a311555ea42a066d29d58218f

[Site Isolation] Use the GoToBackForwardItem message to load history state into iframe processes https://bugs.webkit.org/show_bug.cgi?id=283925 rdar://140802902
Reviewed by Alex Christensen.

Now that we can load history into web processes using only UI process session state, we can use the
`GoToBackForwardItem` IPC message to load history state into iframe processes instead of `LoadRequest`.

Also fix an issue introduced in 285517@main, revealed by this change, where all back/forward items were
unconditionally marked as restored from the session on page creation. Without this fix,
SiteIsolation.NavigateIframeCrossOriginBackForward fails.

No change in behavior with site isolation disabled.

* Source/WebKit/UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::goToBackForwardItem):
(WebKit::WebPageProxy::creationParameters):

Canonical link: https://commits.webkit.org/287253@main

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -2490,21 +2490,9 @@ RefPtr<API::Navigation> WebPageProxy::goToBackForwardItem(WebBackForwardListItem
 
     if (item.isRemoteFrameNavigation() || frameItem.identifier().processIdentifier() != item.rootFrameItem().identifier().processIdentifier()) {
         ASSERT(m_preferences->siteIsolationEnabled());
-        if (RefPtr processForIdentifier = WebProcessProxy::processForIdentifier(frameItem.identifier().processIdentifier()))
-            process = processForIdentifier.releaseNonNull();
-        else {
-            if (&item != m_backForwardList->currentItem())
-                protectedBackForwardList()->goToItem(item);
-            if (RefPtr frame = WebFrameProxy::webFrame(frameItem.frameID())) {
-                LoadParameters loadParameters;
-                loadParameters.request = ResourceRequest { frameItem.frameState().urlString };
-                loadParameters.frameIdentifier = frameItem.frameID();
-                loadParameters.lockBackForwardList = LockBackForwardList::Yes;
-                frame->setPendingChildBackForwardItem(frameItem.parent());
-                Ref frameProcess = frame->process();
-                frameProcess->send(Messages::WebPage::LoadRequest(WTFMove(loadParameters)), webPageIDInProcess(frameProcess));
-                return RefPtr<API::Navigation> { WTFMove(navigation) };
-            }
+        if (RefPtr frame = WebFrameProxy::webFrame(frameItem.frameID())) {
+            frame->setPendingChildBackForwardItem(frameItem.parent());
+            process = frame->process();
         }
     }
 
@@ -11107,7 +11095,8 @@ WebPageCreationParameters WebPageProxy::creationParameters(WebProcessProxy& proc
         userContentController = userContentControllerFromWebsitePolicies.releaseNonNull();
     process.addWebUserContentControllerProxy(userContentController);
 
-    protectedBackForwardList()->setItemsAsRestoredFromSession();
+    if (m_sessionStateWasRestoredByAPIRequest)
+        protectedBackForwardList()->setItemsAsRestoredFromSession();
 
     RefPtr pageClient = this->pageClient();
 