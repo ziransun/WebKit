ca392203b6f570c545629e892a8378093526df99
Philippe Normand <philn@igalia.com>
2024-12-03 00:37:26 -0800

    [GStreamer][WebRTC] Discard audio buffers on incoming tracks until the first video buffer was emitted https://bugs.webkit.org/show_bug.cgi?id=283895
    Reviewed by Xabier Rodriguez-Calvar.
    
    Reduce audio delay caused by initial late video frame.
    
    Based on patch by Manoj Bhatta <manoj_bhatta@comcast.com>.
    
    * Source/WebCore/platform/mediastream/gstreamer/GStreamerMediaStreamSource.cpp:
    (InternalSource::updateFirstVideoSampleSeenFlag):
    (InternalSource::receivedAudioSampleBeforeVideo):
    
    Canonical link: https://commits.webkit.org/287265@main

--- Source/WebCore/platform/mediastream/gstreamer/GStreamerMediaStreamSource.cpp
+++ Source/WebCore/platform/mediastream/gstreamer/GStreamerMediaStreamSource.cpp
@@ -473,6 +473,8 @@ public:
         if (!m_parent || !m_isObserving)
             return;
 
+        updateFirstVideoSampleSeenFlag();
+
         auto videoFrameSize = videoFrame.presentationSize();
         IntSize captureSize(videoFrameSize.width(), videoFrameSize.height());
 
@@ -532,6 +534,9 @@ public:
         if (!m_track)
             return;
 
+        if (receivedAudioSampleBeforeVideo())
+            return;
+
         const auto& data = static_cast<const GStreamerAudioData&>(audioData);
         if (m_track->enabled()) {
             GRefPtr<GstSample> sample = data.getSample();
@@ -574,6 +579,9 @@ public:
     bool isEnded() const { return m_isEnded; }
 
     GstStream* stream() const { return m_stream.get(); }
+
+    void updateFirstVideoSampleSeenFlag();
+    bool receivedAudioSampleBeforeVideo();
 
 private:
     // CheckedPtr interface
@@ -705,6 +713,7 @@ struct _WebKitMediaStreamSrcPrivate {
     Atomic<unsigned> audioPadCounter;
     Atomic<unsigned> videoPadCounter;
     unsigned groupId;
+    bool firstVideoSampleSeen { false };
 };
 
 enum {
@@ -712,6 +721,27 @@ enum {
     PROP_IS_LIVE,
     PROP_LAST
 };
+
+void InternalSource::updateFirstVideoSampleSeenFlag()
+{
+    auto src = WEBKIT_MEDIA_STREAM_SRC_CAST(m_parent);
+    src->priv->firstVideoSampleSeen = true;
+}
+
+bool InternalSource::receivedAudioSampleBeforeVideo()
+{
+    auto src = WEBKIT_MEDIA_STREAM_SRC_CAST(m_parent);
+
+    if (src->priv->firstVideoSampleSeen)
+        return false;
+
+    for (auto& track : src->priv->tracks) {
+        if (track->isVideo())
+            return true;
+    }
+
+    return false;
+}
 
 void WebKitMediaStreamObserver::activeStatusChanged()
 {
