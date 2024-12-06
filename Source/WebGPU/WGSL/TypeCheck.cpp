acc6013cc7d131e651562ee3c17d9bf57f7978c5
Tadeu Zagallo <tzagallo@apple.com>
2024-12-06 05:57:08 -0800

    [WGSL] webgpu:shader,validation,types,textures:* is failing https://bugs.webkit.org/show_bug.cgi?id=284050 rdar://140923477
    Reviewed by Mike Wyrzykowski.
    
    According to the spec[1], using an invalid texel format should not be a shader
    creation error, which was happening before as the `bgra8unorm` value was conditionally
    defined based on the feature being available, which raised a type error at shader
    creation time.
    
    [1]: https://www.w3.org/TR/WGSL/#texture-storage
    
    * Source/WebGPU/WGSL/TypeCheck.cpp:
    (WGSL::TypeChecker::TypeChecker):
    
    Canonical link: https://commits.webkit.org/287444@main

--- Source/WebGPU/WGSL/TypeCheck.cpp
+++ Source/WebGPU/WGSL/TypeCheck.cpp
@@ -407,9 +407,7 @@ TypeChecker::TypeChecker(ShaderModule& shaderModule)
     introduceValue(AST::Identifier::make("write"_s), m_types.accessModeType());
     introduceValue(AST::Identifier::make("read_write"_s), m_types.accessModeType());
 
-    if (m_shaderModule.hasFeature("bgra8unorm-storage"_s))
-        introduceValue(AST::Identifier::make("bgra8unorm"_s), m_types.texelFormatType());
-
+    introduceValue(AST::Identifier::make("bgra8unorm"_s), m_types.texelFormatType());
     introduceValue(AST::Identifier::make("r32float"_s), m_types.texelFormatType());
     introduceValue(AST::Identifier::make("r32sint"_s), m_types.texelFormatType());
     introduceValue(AST::Identifier::make("r32uint"_s), m_types.texelFormatType());
