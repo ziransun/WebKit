e301beb9ed820ee0eef0747aa79303b93b1807a4
Antti Koivisto <antti@apple.com>
2024-12-09 06:38:00 -0800

    [IFC][SVG text] Use iterator in SVGTextQuery https://bugs.webkit.org/show_bug.cgi?id=284282 rdar://141144163
    Reviewed by Alan Baradlay.
    
    This type implements some SVG API text measuring functions.
    
    * Source/WebCore/rendering/svg/SVGTextMetrics.cpp:
    (WebCore::SVGTextMetrics::SVGTextMetrics):
    (WebCore::SVGTextMetrics::constructTextRun):
    (WebCore::SVGTextMetrics::measureCharacterRange):
    * Source/WebCore/rendering/svg/SVGTextMetrics.h:
    * Source/WebCore/rendering/svg/SVGTextQuery.cpp:
    (WebCore::inlineBoxForRenderer):
    (WebCore::SVGTextQuery::SVGTextQuery):
    (WebCore::SVGTextQuery::collectTextBoxesInInlineBox):
    (WebCore::SVGTextQuery::mapStartEndPositionsIntoFragmentCoordinates const):
    (WebCore::SVGTextQuery::modifyStartEndPositionsRespectingLigatures const):
    (WebCore::SVGTextQuery::Data::Data): Deleted.
    (WebCore::flowBoxForRenderer): Deleted.
    (WebCore::SVGTextQuery::collectTextBoxesInFlowBox): Deleted.
    * Source/WebCore/rendering/svg/SVGTextQuery.h:
    
    Canonical link: https://commits.webkit.org/287545@main

--- Source/WebCore/rendering/svg/SVGTextMetrics.cpp
+++ Source/WebCore/rendering/svg/SVGTextMetrics.cpp
@@ -25,7 +25,7 @@
 
 namespace WebCore {
 
-SVGTextMetrics::SVGTextMetrics(RenderSVGInlineText& textRenderer, const TextRun& run)
+SVGTextMetrics::SVGTextMetrics(const RenderSVGInlineText& textRenderer, const TextRun& run)
 {
     float scalingFactor = textRenderer.scalingFactor();
     ASSERT(scalingFactor);
@@ -43,7 +43,7 @@ SVGTextMetrics::SVGTextMetrics(RenderSVGInlineText& textRenderer, const TextRun&
     m_length = static_cast<unsigned>(run.length());
 }
 
-TextRun SVGTextMetrics::constructTextRun(RenderSVGInlineText& text, unsigned position, unsigned length)
+TextRun SVGTextMetrics::constructTextRun(const RenderSVGInlineText& text, unsigned position, unsigned length)
 {
     const RenderStyle& style = text.style();
 
@@ -59,12 +59,12 @@ TextRun SVGTextMetrics::constructTextRun(RenderSVGInlineText& text, unsigned pos
     return run;
 }
 
-SVGTextMetrics SVGTextMetrics::measureCharacterRange(RenderSVGInlineText& text, unsigned position, unsigned length)
+SVGTextMetrics SVGTextMetrics::measureCharacterRange(const RenderSVGInlineText& text, unsigned position, unsigned length)
 {
     return SVGTextMetrics(text, constructTextRun(text, position, length));
 }
 
-SVGTextMetrics::SVGTextMetrics(RenderSVGInlineText& text, unsigned length, float width)
+SVGTextMetrics::SVGTextMetrics(const RenderSVGInlineText& text, unsigned length, float width)
 {
     float scalingFactor = text.scalingFactor();
     ASSERT(scalingFactor);
