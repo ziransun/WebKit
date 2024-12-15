ziransun/WebKit

commit 10ea3bdfd9009b070aa95cb6e46e7948f0b1b7f6

Extend AudioSampleBufferCompressor capabilities to allow for any CMSampleBuffer conversion https://bugs.webkit.org/show_bug.cgi?id=283905 rdar://140782536
Reviewed by Youenn Fablet.

We extend the class to be able to decode various format into PCM as required by the WebCodec's AudioDecoderCocoa class
(WebCodec's AudioEncoderCocoa).
We also rewrite the AudioConverterComplexInputDataProc used by processSample using
the available CoreMedia methods and how we create a CMSampleBuffer. This new version is significantly
smaller and less complex while being able to handle more format (in particular, in can now handle any planar formats and not a maximum of two).

The addSampleBuffer method now returns a GenericPromise which is resolved
once the sampleBuffer has been fulled decoded and all data has been returned
to the callback.
Decoding or Encoding errors are now reported back to the consumer/

We add a new mode where the converter will now longer generate the timestamps but
instead set the timestamp of the input to be the same as the one on the input
(this is an AudioDecoder requirement)

This change will be fully tested in a follow-up commit where we introduce AudioDecoderCocoa.
For now, there's no impact on its only consumer: MediaRecorderPrivateEncoder
which is covered by existing tests.

Fly-By: change use of `auto` for explicit `int` when reporting error as it causes compilation error when using OSStatus.

* Source/WebCore/platform/audio/cocoa/AudioFileReaderCocoa.cpp:
(WebCore::AudioFileReader::getPacketDescriptions const): Deleted.
* Source/WebCore/platform/audio/cocoa/AudioFileReaderCocoa.h:
* Source/WebCore/platform/graphics/cocoa/CMUtilities.h: Extract getPacketDescriptions method from AudioFileReaderCocoa.
* Source/WebCore/platform/graphics/cocoa/CMUtilities.mm:
(WebCore::getPacketDescriptions):
* Source/WebCore/platform/mediarecorder/cocoa/AudioSampleBufferCompressor.h:
(WebCore::AudioSampleBufferCompressor::isPCM const):

Source/WebCore/platform/graphics/cocoa/CMUtilities.mm
@@ -436,6 +436,30 @@ void PacketDurationParser::reset()
 
 PacketDurationParser::~PacketDurationParser() = default;
 
+Vector<AudioStreamPacketDescription> getPacketDescriptions(CMSampleBufferRef sampleBuffer)
+{
+    size_t packetDescriptionsSize;
+    if (PAL::CMSampleBufferGetAudioStreamPacketDescriptions(sampleBuffer, 0, nullptr, &packetDescriptionsSize) != noErr) {
+        RELEASE_LOG_FAULT(WebAudio, "Unable to get packet description list size");
+        return { };
+    }
+    size_t numDescriptions = packetDescriptionsSize / sizeof(AudioStreamPacketDescription);
+    if (!numDescriptions) {
+        RELEASE_LOG_FAULT(WebAudio, "No packet description found.");
+        return { };
+    }
+    Vector<AudioStreamPacketDescription> descriptions(numDescriptions);
+    if (PAL::CMSampleBufferGetAudioStreamPacketDescriptions(sampleBuffer, packetDescriptionsSize, descriptions.data(), nullptr) != noErr) {
+        RELEASE_LOG_FAULT(WebAudio, "Unable to get packet description list");
+        return { };
+    }
+    auto numPackets = PAL::CMSampleBufferGetNumSamples(sampleBuffer);
+    if (numDescriptions != size_t(numPackets)) {
+        RELEASE_LOG_FAULT(WebAudio, "Unhandled CMSampleBuffer structure");
+        return { };
+    }
+    return descriptions;
+}
 
 } // namespace WebCore
 

Source/WebCore/platform/audio/cocoa/AudioFileReaderCocoa.cpp
@@ -36,6 +36,7 @@
 #include "AudioFileReader.h"
 #include "AudioSampleDataSource.h"
 #include "AudioTrackPrivateWebM.h"
+#include "CMUtilities.h"
 #include "FloatConversion.h"
 #include "InbandTextTrackPrivate.h"
 #include "Logging.h"
@@ -275,31 +276,6 @@ static OSStatus passthroughInputDataCallback(AudioConverterRef, UInt32* numDataP
     userData->m_index++;
 
     return noErr;
-}
-
-Vector<AudioStreamPacketDescription> AudioFileReader::getPacketDescriptions(CMSampleBufferRef sampleBuffer) const
-{
-    size_t packetDescriptionsSize;
-    if (PAL::CMSampleBufferGetAudioStreamPacketDescriptions(sampleBuffer, 0, nullptr, &packetDescriptionsSize) != noErr) {
-        RELEASE_LOG_FAULT(WebAudio, "Unable to get packet description list size");
-        return { };
-    }
-    size_t numDescriptions = packetDescriptionsSize / sizeof(AudioStreamPacketDescription);
-    if (!numDescriptions) {
-        RELEASE_LOG_FAULT(WebAudio, "No packet description found.");
-        return { };
-    }
-    Vector<AudioStreamPacketDescription> descriptions(numDescriptions);
-    if (PAL::CMSampleBufferGetAudioStreamPacketDescriptions(sampleBuffer, packetDescriptionsSize, descriptions.data(), nullptr) != noErr) {
-        RELEASE_LOG_FAULT(WebAudio, "Unable to get packet description list");
-        return { };
-    }
-    auto numPackets = PAL::CMSampleBufferGetNumSamples(sampleBuffer);
-    if (numDescriptions != size_t(numPackets)) {
-        RELEASE_LOG_FAULT(WebAudio, "Unhandled CMSampleBuffer structure");
-        return { };
-    }
-    return descriptions;
 }
 
 std::optional<size_t> AudioFileReader::decodeWebMData(AudioBufferList& bufferList, size_t numberOfFrames, const AudioStreamBasicDescription& inFormat, const AudioStreamBasicDescription& outFormat) const

Source/WebCore/platform/audio/cocoa/AudioFileReaderCocoa.h
@@ -73,7 +73,6 @@ private:
 #if ENABLE(MEDIA_SOURCE)
     bool isMaybeWebM(std::span<const uint8_t>) const;
     std::unique_ptr<AudioFileReaderWebMData> demuxWebMData(std::span<const uint8_t>) const;
-    Vector<AudioStreamPacketDescription> getPacketDescriptions(CMSampleBufferRef) const;
     std::optional<size_t> decodeWebMData(AudioBufferList&, size_t numberOfFrames, const AudioStreamBasicDescription& inFormat, const AudioStreamBasicDescription& outFormat) const;
 #endif
     static OSStatus readProc(void* clientData, SInt64 position, UInt32 requestCount, void* buffer, UInt32* actualCount);

Source/WebCore/platform/graphics/cocoa/CMUtilities.h
@@ -27,6 +27,7 @@
 
 #if PLATFORM(COCOA)
 
+#include <CoreAudio/CoreAudioTypes.h>
 #include <memory>
 #include <wtf/Expected.h>
 #include <wtf/RetainPtr.h>
@@ -74,6 +75,8 @@ private:
     uint32_t m_lastVorbisBlockSize { 0 };
     bool m_isValid { false };
 };
+
+Vector<AudioStreamPacketDescription> getPacketDescriptions(CMSampleBufferRef);
 
 }
 

Source/WebCore/platform/mediarecorder/MediaRecorderPrivateEncoder.cpp
@@ -317,15 +317,18 @@ void MediaRecorderPrivateEncoder::audioSamplesDescriptionChanged(const AudioStre
         m_formatChangedOccurred = true;
     }
 
-    m_audioCompressor = AudioSampleBufferCompressor::create(compressedAudioOutputBufferCallback, this, m_audioCodec, *m_originalOutputDescription);
+    AudioSampleBufferCompressor::Options options = {
+        .format = m_audioCodec,
+        .description = m_originalOutputDescription,
+        .outputBitRate = m_audioBitsPerSecond ? std::optional { m_audioBitsPerSecond } : std::nullopt,
+        .generateTimestamp = true
+    };
+    m_audioCompressor = AudioSampleBufferCompressor::create(compressedAudioOutputBufferCallback, this, options);
     if (!m_audioCompressor) {
         RELEASE_LOG_ERROR(MediaStream, "MediaRecorderPrivateEncoder::audioSamplesDescriptionChanged: creation of compressor failed");
         m_hadError = true;
         return;
     }
-
-    if (m_audioBitsPerSecond)
-        audioCompressor()->setBitsPerSecond(m_audioBitsPerSecond);
 
     updateCurrentRingBufferIfNeeded();
 }

Source/WebCore/platform/mediarecorder/cocoa/AudioSampleBufferCompressor.h
@@ -24,7 +24,7 @@
 
 #pragma once
 
-#if ENABLE(MEDIA_RECORDER) && USE(AVFOUNDATION)
+#if USE(AVFOUNDATION)
 
 #include <CoreMedia/CoreMedia.h>
 #include <wtf/Forward.h>
@@ -37,24 +37,31 @@ OBJC_CLASS NSNumber;
 
 namespace WebCore {
 
-class AudioSampleBufferCompressor : public ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<AudioSampleBufferCompressor, WTF::DestructionThread::Main> {
+class WebAudioBufferList;
+
+class AudioSampleBufferCompressor : public ThreadSafeRefCountedAndCanMakeThreadSafeWeakPtr<AudioSampleBufferCompressor> {
 public:
-    static RefPtr<AudioSampleBufferCompressor> create(CMBufferQueueTriggerCallback, void* callbackObject, AudioFormatID, std::optional<AudioStreamBasicDescription>);
+    struct Options {
+        AudioFormatID format { kAudioFormatMPEG4AAC };
+        std::optional<AudioStreamBasicDescription> description;
+        std::optional<unsigned> outputBitRate;
+        bool generateTimestamp { true };
+    };
+    static RefPtr<AudioSampleBufferCompressor> create(CMBufferQueueTriggerCallback, void* callbackObject, const Options&);
     ~AudioSampleBufferCompressor();
 
     bool isEmpty() const;
-    void setBitsPerSecond(unsigned);
     Ref<GenericPromise> finish() { return flushInternal(true); }
     Ref<GenericPromise> flush() { return flushInternal(false); }
     Ref<GenericPromise> drain();
-    void addSampleBuffer(CMSampleBufferRef);
+    Ref<GenericPromise> addSampleBuffer(CMSampleBufferRef);
     CMSampleBufferRef getOutputSampleBuffer() const;
     RetainPtr<CMSampleBufferRef> takeOutputSampleBuffer();
 
     unsigned bitRate() const;
 
 private:
-    AudioSampleBufferCompressor(AudioFormatID, std::optional<AudioStreamBasicDescription>&&);
+    AudioSampleBufferCompressor(const Options&);
     bool initialize(CMBufferQueueTriggerCallback, void* callbackObject);
     UInt32 defaultOutputBitRate(const AudioStreamBasicDescription&) const;
 
@@ -64,40 +71,43 @@ private:
     bool initAudioConverterForSourceFormatDescription(CMFormatDescriptionRef, AudioFormatID);
     void attachPrimingTrimsIfNeeded(CMSampleBufferRef);
     RetainPtr<NSNumber> gradualDecoderRefreshCount();
-    RetainPtr<CMSampleBufferRef> sampleBuffer(AudioBufferList);
+    Expected<RetainPtr<CMSampleBufferRef>, int> sampleBuffer(const WebAudioBufferList&, uint32_t numSamples);
     void processSampleBuffers();
     OSStatus provideSourceDataNumOutputPackets(UInt32*, AudioBufferList*, AudioStreamPacketDescription**);
     Ref<GenericPromise> flushInternal(bool isFinished);
 
-    Ref<WorkQueue> m_serialDispatchQueue;
-    CMTime m_lowWaterTime;
+    bool isPCM() const;
+    void setTimeFromSample(CMSampleBufferRef);
 
-    RetainPtr<CMBufferQueueRef> m_outputBufferQueue;
-    RetainPtr<CMBufferQueueRef> m_inputBufferQueue;
-    bool m_isEncoding { false };
-    bool m_isDraining { false };
+    Ref<WorkQueue> queue() const { return m_serialDispatchQueue; }
+    const Ref<WorkQueue> m_serialDispatchQueue;
 
-    AudioConverterRef m_converter { nullptr };
-    AudioStreamBasicDescription m_sourceFormat;
-    AudioStreamBasicDescription m_destinationFormat;
-    RetainPtr<CMFormatDescriptionRef> m_destinationFormatDescription;
-    RetainPtr<NSNumber> m_gdrCountNum;
-    UInt32 m_maxOutputPacketSize { 0 };
-    AudioStreamPacketDescription m_destinationPacketDescriptions;
+    RetainPtr<CMBufferQueueRef> m_outputBufferQueue; // initialized on the caller's thread once, never modified after that.
+    RetainPtr<CMBufferQueueRef> m_inputBufferQueue; // initialized on the caller's thread once, never modified after that.
+    bool m_isEncoding WTF_GUARDED_BY_CAPABILITY(queue().get()) { false };
+    bool m_isDraining WTF_GUARDED_BY_CAPABILITY(queue().get()) { false };
 
-    CMTime m_currentNativePresentationTimeStamp;
-    CMTime m_currentOutputPresentationTimeStamp;
-    CMTime m_remainingPrimeDuration;
+    AudioConverterRef m_converter WTF_GUARDED_BY_CAPABILITY(queue().get()) { nullptr };
+    AudioStreamBasicDescription m_sourceFormat WTF_GUARDED_BY_CAPABILITY(queue().get());
+    AudioStreamBasicDescription m_destinationFormat WTF_GUARDED_BY_CAPABILITY(queue().get());
+    RetainPtr<CMFormatDescriptionRef> m_destinationFormatDescription WTF_GUARDED_BY_CAPABILITY(queue().get());
+    RetainPtr<NSNumber> m_gdrCountNum WTF_GUARDED_BY_CAPABILITY(queue().get());
+    UInt32 m_maxOutputPacketSize WTF_GUARDED_BY_CAPABILITY(queue().get()) { 0 };
+    Vector<AudioStreamPacketDescription> m_destinationPacketDescriptions WTF_GUARDED_BY_CAPABILITY(queue().get());
 
-    Vector<uint8_t> m_sourceBuffer;
-    Vector<uint8_t> m_destinationBuffer;
+    CMTime m_currentNativePresentationTimeStamp WTF_GUARDED_BY_CAPABILITY(queue().get());
+    CMTime m_currentOutputPresentationTimeStamp WTF_GUARDED_BY_CAPABILITY(queue().get());
+    CMTime m_remainingPrimeDuration WTF_GUARDED_BY_CAPABILITY(queue().get());
+
+    Vector<uint8_t> m_destinationBuffer WTF_GUARDED_BY_CAPABILITY(queue().get());
 
     CMBufferQueueTriggerToken m_triggerToken;
-    RetainPtr<CMBlockBufferRef> m_sampleBlockBuffer;
-    size_t m_sampleBlockBufferSize { 0 };
-    size_t m_currentOffsetInSampleBlockBuffer { 0 };
+    RetainPtr<CMBlockBufferRef> m_blockBuffer WTF_GUARDED_BY_CAPABILITY(queue().get());
+    Vector<AudioStreamPacketDescription> m_packetDescriptions WTF_GUARDED_BY_CAPABILITY(queue().get());
+    int m_lastError WTF_GUARDED_BY_CAPABILITY(queue().get()) { 0 };
     const AudioFormatID m_outputCodecType;
-    std::optional<unsigned> m_outputBitRate;
+    const std::optional<unsigned> m_outputBitRate;
+    const bool m_generateTimestamp { true };
 };
 
 }

commit d6d9a451a70c766a8c379ee933028fb12aeefebe

AudioSampleBufferConverter Incorrectly set number of out packets to 1 https://bugs.webkit.org/show_bug.cgi?id=284702 rdar://141499128
Reviewed by Eric Carlson.

Regression introduced in 287362@main.
We partially revert back to the proper frame calculation when the source is PCM.

Also change the log level in CMUtilities getPacketDescriptions as it's unnecessary verbose
when used to compress PCM data.

* Source/WebCore/platform/audio/cocoa/AudioSampleBufferConverter.mm:
(WebCore::AudioSampleBufferConverter::provideSourceDataNumOutputPackets):

Canonical link: https://commits.webkit.org/287850@main

Source/WebCore/platform/audio/cocoa/AudioSampleBufferConverter.mm
@@ -441,6 +441,9 @@ OSStatus AudioSampleBufferConverter::provideSourceDataNumOutputPackets(UInt32* n
     if (packetDescriptionOut) {
         *numOutputPacketsPtr = m_packetDescriptions.size();
         *packetDescriptionOut = m_packetDescriptions.data();
+    } else if (m_sourceFormat.mFormatID == kAudioFormatLinearPCM) {
+        ASSERT(audioBufferList->mNumberBuffers && m_sourceFormat.mBytesPerPacket);
+        *numOutputPacketsPtr = (audioBufferList->mBuffers[0].mDataByteSize / m_sourceFormat.mBytesPerPacket);
     } else
         *numOutputPacketsPtr = 1;
 

Source/WebCore/platform/graphics/cocoa/CMUtilities.mm
@@ -440,22 +440,22 @@ Vector<AudioStreamPacketDescription> getPacketDescriptions(CMSampleBufferRef sam
 {
     size_t packetDescriptionsSize;
     if (PAL::CMSampleBufferGetAudioStreamPacketDescriptions(sampleBuffer, 0, nullptr, &packetDescriptionsSize) != noErr) {
-        RELEASE_LOG_FAULT(WebAudio, "Unable to get packet description list size");
+        RELEASE_LOG_FAULT(Media, "Unable to get packet description list size");
         return { };
     }
     size_t numDescriptions = packetDescriptionsSize / sizeof(AudioStreamPacketDescription);
     if (!numDescriptions) {
-        RELEASE_LOG_FAULT(WebAudio, "No packet description found.");
+        RELEASE_LOG_DEBUG(Media, "No packet description found.");
         return { };
     }
     Vector<AudioStreamPacketDescription> descriptions(numDescriptions);
     if (PAL::CMSampleBufferGetAudioStreamPacketDescriptions(sampleBuffer, packetDescriptionsSize, descriptions.data(), nullptr) != noErr) {
-        RELEASE_LOG_FAULT(WebAudio, "Unable to get packet description list");
+        RELEASE_LOG_FAULT(Media, "Unable to get packet description list");
         return { };
     }
     auto numPackets = PAL::CMSampleBufferGetNumSamples(sampleBuffer);
     if (numDescriptions != size_t(numPackets)) {
-        RELEASE_LOG_FAULT(WebAudio, "Unhandled CMSampleBuffer structure");
+        RELEASE_LOG_FAULT(Media, "Unhandled CMSampleBuffer structure");
         return { };
     }
     return descriptions;