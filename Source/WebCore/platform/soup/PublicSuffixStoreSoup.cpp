a8b6ea5c059f5f7e9f4ad18af1dbc29c60954286
Philippe Normand <philn@igalia.com>
2024-12-15 10:28:12 -0800

    REGRESSION(287061@main): [SOUP] Prevent early destruction of the tld string in PublicSuffixStore https://bugs.webkit.org/show_bug.cgi?id=284711
    Reviewed by Michael Catanzaro.
    
    Make sure the string passed to soup_tld_get_base_domain() is not destroyed before usage.
    
    Canonical link: https://commits.webkit.org/287853@main

--- Source/WebCore/platform/soup/PublicSuffixStoreSoup.cpp
+++ Source/WebCore/platform/soup/PublicSuffixStoreSoup.cpp
@@ -68,13 +68,16 @@ String PublicSuffixStore::platformTopPrivatelyControlledDomain(StringView domain
     if (position == domain.length())
         return String();
 
+    auto tldView = domain.substring(position);
+    const auto tldCString = tldView.utf8();
+
     GUniqueOutPtr<GError> error;
-    if (const char* baseDomain = soup_tld_get_base_domain(domain.substring(position).utf8().data(), &error.outPtr()))
+    if (const char* baseDomain = soup_tld_get_base_domain(tldCString.data(), &error.outPtr()))
         return String::fromUTF8(baseDomain);
 
     if (g_error_matches(error.get(), SOUP_TLD_ERROR, SOUP_TLD_ERROR_NO_BASE_DOMAIN)) {
         if (domain.endsWithIgnoringASCIICase("web-platform.test"_s))
-            return permissiveTopPrivateDomain(domain.substring(position));
+            return permissiveTopPrivateDomain(tldView);
         return String();
     }
 
