474e17e9c9a8acc6dbb8cc6aed4e9302ae77970e
Tim Horton <thorton@apple.com>
2024-12-17 21:46:52 -0800

    REGRESSION (iOS 18.2): Scrolling does not work on websites built on Flutter https://bugs.webkit.org/show_bug.cgi?id=284859 rdar://140119315
    Reviewed by Wenson Hsieh and Richard Robinson.
    
    "Child" events (coalesced and predicted events) of a given pointer event must
    have the same pointer identifier as their parent; the spec says so, and Flutter
    strongly depends on this.
    
    We did not implement it this way; the child events get a pointer identifier of 0.
    
    * Source/WebKit/UIProcess/ios/WKTouchEventsGestureRecognizer.mm:
    (-[WKTouchEventsGestureRecognizer _touchEventForChildTouch:withParent:]):
    (-[WKTouchEventsGestureRecognizer _recordTouches:ofType:forEvent:]):
    (-[WKTouchEventsGestureRecognizer _processTouches:withEvent:type:]):
    (-[WKTouchEventsGestureRecognizer _touchEventForTouch:]): Deleted.
    (-[WKTouchEventsGestureRecognizer _recordTouches:type:coalescedTouches:predictedTouches:]):
    Instead of adding coalesced and predicted events from a random touch, take them
    from the first touch, and propagate the pointer identifier from the first touch
    onto them.
    
    Later, we should get them from *each* touch instead of just one, to properly
    support multiple touches.
    
    Canonical link: https://commits.webkit.org/287988@main

--- Source/WebKit/UIProcess/ios/WKTouchEventsGestureRecognizer.mm
+++ Source/WebKit/UIProcess/ios/WKTouchEventsGestureRecognizer.mm
@@ -233,7 +233,7 @@ static CGPoint mapRootViewToViewport(CGPoint pointInRootView, WKContentView *con
     return CGPointMake(pointInRootView.x - offsetInRootView.x, pointInRootView.y - offsetInRootView.y);
 }
 
-- (WebKit::WKTouchEvent)_touchEventForTouch:(UITouch *)touch
+- (WebKit::WKTouchEvent)_touchEventForChildTouch:(UITouch *)touch withParent:(const WebKit::WKTouchPoint&)parentTouchPoint
 {
     auto locationInWindow = [touch locationInView:nil];
     auto locationInRootView = [[self view] convertPoint:locationInWindow fromView:nil];
@@ -242,7 +242,7 @@ static CGPoint mapRootViewToViewport(CGPoint pointInRootView, WKContentView *con
     WebKit::WKTouchPoint touchPoint;
     touchPoint.locationInRootViewCoordinates = locationInRootView;
     touchPoint.locationInViewport = mapRootViewToViewport(locationInRootView, contentView.get());
-    touchPoint.identifier = 0;
+    touchPoint.identifier = parentTouchPoint.identifier;
     touchPoint.phase = touch.phase;
     touchPoint.majorRadiusInWindowCoordinates = touch.majorRadius;
     touchPoint.force = touch.maximumPossibleForce > 0 ? touch.force / touch.maximumPossibleForce : 0;
@@ -266,7 +266,7 @@ static CGPoint mapRootViewToViewport(CGPoint pointInRootView, WKContentView *con
     return event;
 }
 
-- (void)_recordTouches:(NSSet<UITouch *> *)touches type:(WebKit::WKTouchEventType)type coalescedTouches:(NSArray<UITouch *> *)coalescedTouches predictedTouches:(NSArray<UITouch *> *)predictedTouches
+- (void)_recordTouches:(NSSet<UITouch *> *)touches ofType:(WebKit::WKTouchEventType)type forEvent:(UIEvent *)event
 {
     _lastTouchEvent.type = type;
     _lastTouchEvent.inJavaScriptGesture = false;
@@ -290,14 +290,6 @@ static CGPoint mapRootViewToViewport(CGPoint pointInRootView, WKContentView *con
 
     _lastTouchEvent.coalescedEvents = { };
     _lastTouchEvent.predictedEvents = { };
-
-    if (type == WebKit::WKTouchEventType::Change) {
-        for (UITouch *coalescedTouch in coalescedTouches)
-            _lastTouchEvent.coalescedEvents.append([self _touchEventForTouch:coalescedTouch]);
-
-        for (UITouch *predictedTouch in predictedTouches)
-            _lastTouchEvent.predictedEvents.append([self _touchEventForTouch:predictedTouch]);
-    }
 
     RetainPtr contentView = [self contentView];
     NSUInteger touchIndex = 0;
@@ -346,6 +338,15 @@ static CGPoint mapRootViewToViewport(CGPoint pointInRootView, WKContentView *con
             touchPoint.touchType = WebKit::WKTouchPointType::Direct;
             touchPoint.altitudeAngle = 0;
             touchPoint.azimuthAngle = 0;
+        }
+
+        // FIXME (284852): Instead of retrieving coalesced and predicted touches from the first touch, we should store them per-touch.
+        if (!touchIndex && type == WebKit::WKTouchEventType::Change) {
+            for (UITouch *coalescedTouch in [event coalescedTouchesForTouch:touch])
+                _lastTouchEvent.coalescedEvents.append([self _touchEventForChildTouch:coalescedTouch withParent:touchPoint]);
+
+            for (UITouch *predictedTouch in [event predictedTouchesForTouch:touch])
+                _lastTouchEvent.predictedEvents.append([self _touchEventForChildTouch:predictedTouch withParent:touchPoint]);
         }
 
         ++touchIndex;
@@ -468,7 +469,7 @@ static WebKit::WKTouchEventType lastExpectedWKEventTypeForTouches(NSSet *touches
     if (lastExpectedWKEventTypeForTouches(touches) != type)
         return;
 
-    [self _recordTouches:touches type:type coalescedTouches:[event coalescedTouchesForTouch:touches.anyObject] predictedTouches:[event predictedTouchesForTouch:touches.anyObject]];
+    [self _recordTouches:touches ofType:type forEvent:event];
 
     [self performAction];
 
