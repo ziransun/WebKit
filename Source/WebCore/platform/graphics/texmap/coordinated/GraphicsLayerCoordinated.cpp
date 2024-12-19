ziransun/WebKit

commit d81c1b27d2ba489e7f086c79af5da4235e0308ee

[CoordinatedGraphics] Use CoordinatedPlatformLayer in async scrolling https://bugs.webkit.org/show_bug.cgi?id=284518
Reviewed by Alejandro G. Castro.

* Source/WebCore/page/scrolling/ScrollingPlatformLayer.h:
* Source/WebCore/page/scrolling/ScrollingTree.h:
(WebCore::ScrollingTree::isScrollingTreeNicosia const):
* Source/WebCore/page/scrolling/nicosia/ScrollingCoordinatorNicosia.cpp:
(WebCore::ScrollingCoordinatorNicosia::didCompletePlatformRenderingUpdate):
* Source/WebCore/page/scrolling/nicosia/ScrollingStateNodeNicosia.cpp:
(WebCore::LayerRepresentation::makePlatformLayerTyped):
(WebCore::LayerRepresentation::makePlatformLayerTypeless):
(WebCore::LayerRepresentation::platformLayerFromGraphicsLayer):
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeFixedNodeNicosia.cpp:
(WebCore::ScrollingTreeFixedNodeNicosia::commitStateBeforeChildren):
(WebCore::ScrollingTreeFixedNodeNicosia::applyLayerPositions):
(WebCore::ScrollingTreeFixedNodeNicosia::dumpProperties const):
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeFixedNodeNicosia.h:
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeFrameScrollingNodeNicosia.cpp:
(WebCore::ScrollingTreeFrameScrollingNodeNicosia::commitStateBeforeChildren):
(WebCore::ScrollingTreeFrameScrollingNodeNicosia::repositionScrollingLayers):
(WebCore::ScrollingTreeFrameScrollingNodeNicosia::repositionRelatedLayers):
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeFrameScrollingNodeNicosia.h:
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeNicosia.cpp:
(WebCore::ScrollingTreeNicosia::applyLayerPositionsInternal):
(WebCore::ScrollingTreeNicosia::didCompleteRenderingUpdate):
(WebCore::ScrollingTreeNicosia::didCompletePlatformRenderingUpdate):
(WebCore::collectDescendantLayersAtPoint):
(WebCore::ScrollingTreeNicosia::scrollingNodeForPoint):
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeNicosia.h:
* Source/WebCore/page/scrolling/nicosia/ScrollingTreeOverflowScrollProxyNodeNicosia.cpp:

Source/WebCore/platform/graphics/texmap/coordinated/GraphicsLayerCoordinated.cpp
@@ -139,8 +139,6 @@ void GraphicsLayerCoordinated::syncPosition(const FloatPoint& position)
 
     GraphicsLayer::syncPosition(position);
 
-    // FIXME: remove this when async scrolling uses CoordinatedPlatformLayer.
-    m_platformLayer->syncPosition(m_position);
     // Ensure future flushes will recompute the coverage rect and update tiling.
     noteLayerPropertyChanged(Change::TileCoverage, ScheduleFlush::No);
     setNeedsUpdateLayerTransform();
@@ -163,8 +161,6 @@ void GraphicsLayerCoordinated::syncBoundsOrigin(const FloatPoint& origin)
 
     GraphicsLayer::syncBoundsOrigin(origin);
 
-    // FIXME: remove this when async scrolling uses CoordinatedPlatformLayer.
-    m_platformLayer->syncBoundsOrigin(m_boundsOrigin);
     // Ensure future flushes will recompute the coverage rect and update tiling.
     noteLayerPropertyChanged(Change::TileCoverage, ScheduleFlush::No);
     setNeedsUpdateLayerTransform();

Source/WebCore/page/scrolling/ScrollingPlatformLayer.h
@@ -25,18 +25,13 @@
 
 #pragma once
 
-#if USE(NICOSIA)
-namespace Nicosia {
-class CompositionLayer;
-}
-#else
 #include "PlatformLayer.h"
-#endif
 
 namespace WebCore {
 
-#if USE(NICOSIA)
-using ScrollingPlatformLayer = Nicosia::CompositionLayer;
+#if USE(COORDINATED_GRAPHICS)
+class CoordinatedPlatformLayer;
+using ScrollingPlatformLayer = CoordinatedPlatformLayer;
 #else
 using ScrollingPlatformLayer = PlatformLayer;
 #endif

Source/WebCore/page/scrolling/ScrollingTree.h
@@ -75,6 +75,7 @@ public:
     virtual bool isScrollingTreeMac() const { return false; }
     virtual bool isRemoteScrollingTree() const { return false; }
     virtual bool isScrollingTreeIOS() const { return false; }
+    virtual bool isScrollingTreeNicosia() const { return false; }
 
     // This implies that we'll do hit-testing in the scrolling tree.
     bool asyncFrameOrOverflowScrollingEnabled() const { return m_asyncFrameOrOverflowScrollingEnabled; }

Source/WebCore/page/scrolling/nicosia/ScrollingCoordinatorNicosia.cpp
@@ -52,7 +52,7 @@ ScrollingCoordinatorNicosia::~ScrollingCoordinatorNicosia()
 
 void ScrollingCoordinatorNicosia::didCompletePlatformRenderingUpdate()
 {
-    downcast<ThreadedScrollingTree>(scrollingTree())->didCompleteRenderingUpdate();
+    downcast<ScrollingTreeNicosia>(scrollingTree())->didCompletePlatformRenderingUpdate();
 }
 
 } // namespace WebCore

Source/WebCore/page/scrolling/nicosia/ScrollingStateNodeNicosia.cpp
@@ -29,7 +29,6 @@
 #if ENABLE(ASYNC_SCROLLING) && USE(NICOSIA)
 #include "CoordinatedPlatformLayer.h"
 #include "GraphicsLayerCoordinated.h"
-#include "NicosiaCompositionLayer.h"
 
 namespace WebCore {
 
@@ -45,19 +44,19 @@ void LayerRepresentation::releasePlatformLayer(void* typelessLayer)
         layer->deref();
 }
 
-Nicosia::CompositionLayer* LayerRepresentation::makePlatformLayerTyped(void* typelessLayer)
+CoordinatedPlatformLayer* LayerRepresentation::makePlatformLayerTyped(void* typelessLayer)
 {
-    return static_cast<Nicosia::CompositionLayer*>(typelessLayer);
+    return static_cast<CoordinatedPlatformLayer*>(typelessLayer);
 }
 
-void* LayerRepresentation::makePlatformLayerTypeless(Nicosia::CompositionLayer* layer)
+void* LayerRepresentation::makePlatformLayerTypeless(CoordinatedPlatformLayer* layer)
 {
     return layer;
 }
 
-Nicosia::CompositionLayer* LayerRepresentation::platformLayerFromGraphicsLayer(GraphicsLayer& graphicsLayer)
+CoordinatedPlatformLayer* LayerRepresentation::platformLayerFromGraphicsLayer(GraphicsLayer& graphicsLayer)
 {
-    return downcast<GraphicsLayerCoordinated>(graphicsLayer).coordinatedPlatformLayer().compositionLayer().get();
+    return &downcast<GraphicsLayerCoordinated>(graphicsLayer).coordinatedPlatformLayer();
 }
 
 } // namespace WebCore

Source/WebCore/page/scrolling/nicosia/ScrollingTreeFixedNodeNicosia.cpp
@@ -30,9 +30,10 @@
 #include "ScrollingTreeFixedNodeNicosia.h"
 
 #if ENABLE(ASYNC_SCROLLING) && USE(NICOSIA)
-
+#include "CoordinatedPlatformLayer.h"
 #include "Logging.h"
 #include "ScrollingStateFixedNode.h"
+#include "ScrollingThread.h"
 #include <wtf/text/TextStream.h>
 
 namespace WebCore {
@@ -56,7 +57,7 @@ bool ScrollingTreeFixedNodeNicosia::commitStateBeforeChildren(const ScrollingSta
 
     auto& fixedStateNode = downcast<ScrollingStateFixedNode>(stateNode);
     if (fixedStateNode.hasChangedProperty(ScrollingStateNode::Property::Layer))
-        m_layer = static_cast<Nicosia::CompositionLayer*>(fixedStateNode.layer());
+        m_layer = static_cast<CoordinatedPlatformLayer*>(fixedStateNode.layer());
 
     return ScrollingTreeFixedNode::commitStateBeforeChildren(stateNode);
 }
@@ -67,13 +68,11 @@ void ScrollingTreeFixedNodeNicosia::applyLayerPositions()
 
     LOG_WITH_STREAM(Scrolling, stream << "ScrollingTreeFixedNode " << scrollingNodeID() << " relatedNodeScrollPositionDidChange: viewportRectAtLastLayout " << m_constraints.viewportRectAtLastLayout() << " last layer pos " << m_constraints.layerPositionAtLastLayout() << " layerPosition " << layerPosition);
 
-    ASSERT(m_layer);
-    m_layer->accessPending(
-        [&layerPosition](Nicosia::CompositionLayer::LayerState& state)
-        {
-            state.position = layerPosition;
-            state.delta.positionChanged = true;
-        });
+    // Match the behavior of ScrollingTreeFrameScrollingNodeNicosia::repositionScrollingLayers().
+    CoordinatedPlatformLayer::ForcePositionSync forceSync = ScrollingThread::isCurrentThread() && !scrollingTree()->isScrollingSynchronizedWithMainThread() ?
+        CoordinatedPlatformLayer::ForcePositionSync::Yes : CoordinatedPlatformLayer::ForcePositionSync::No;
+
+    m_layer->setTopLeftPositionForScrolling(layerPosition - m_constraints.alignmentOffset(), forceSync);
 }
 
 void ScrollingTreeFixedNodeNicosia::dumpProperties(TextStream& ts, OptionSet<ScrollingStateTreeAsTextBehavior> behavior) const
@@ -81,14 +80,7 @@ void ScrollingTreeFixedNodeNicosia::dumpProperties(TextStream& ts, OptionSet<Scr
     ScrollingTreeFixedNode::dumpProperties(ts, behavior);
 
     if (behavior & ScrollingStateTreeAsTextBehavior::IncludeLayerPositions) {
-        FloatPoint layerTopLeft;
-        ASSERT(m_layer);
-        m_layer->accessCommitted(
-            [this, &layerTopLeft](Nicosia::CompositionLayer::LayerState& state)
-            {
-                layerTopLeft = state.position - toFloatSize(state.anchorPoint.xy()) * state.size + m_constraints.alignmentOffset();
-            });
-
+        FloatPoint layerTopLeft = m_layer->topLeftPositionForScrolling() + m_constraints.alignmentOffset();
         ts.dumpProperty("layer top left", layerTopLeft);
     }
 }

Source/WebCore/page/scrolling/nicosia/ScrollingTreeFixedNodeNicosia.h
@@ -30,11 +30,11 @@
 
 #if ENABLE(ASYNC_SCROLLING) && USE(NICOSIA)
 
-#include "NicosiaCompositionLayer.h"
 #include "ScrollingTreeFixedNode.h"
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
+class CoordinatedPlatformLayer;
 
 class ScrollingTreeFixedNodeNicosia final : public ScrollingTreeFixedNode {
 public:
@@ -44,14 +44,14 @@ public:
 private:
     ScrollingTreeFixedNodeNicosia(ScrollingTree&, ScrollingNodeID);
 
-    Nicosia::CompositionLayer* layer() const override { return m_layer.get(); }
+    CoordinatedPlatformLayer* layer() const override { return m_layer.get(); }
 
     bool commitStateBeforeChildren(const ScrollingStateNode&) override;
     void applyLayerPositions() override WTF_REQUIRES_LOCK(scrollingTree()->treeLock());
 
     void dumpProperties(WTF::TextStream&, OptionSet<ScrollingStateTreeAsTextBehavior>) const override;
 
-    RefPtr<Nicosia::CompositionLayer> m_layer;
+    RefPtr<CoordinatedPlatformLayer> m_layer;
 };
 
 } // namespace WebCore

Source/WebCore/page/scrolling/nicosia/ScrollingTreeFrameScrollingNodeNicosia.cpp
@@ -30,11 +30,11 @@
 #include "ScrollingTreeFrameScrollingNodeNicosia.h"
 
 #if ENABLE(ASYNC_SCROLLING) && USE(NICOSIA)
-
+#include "CoordinatedPlatformLayer.h"
 #include "LocalFrameView.h"
 #include "Logging.h"
-#include "NicosiaCompositionLayer.h"
 #include "ScrollingStateFrameScrollingNode.h"
+#include "ScrollingThread.h"
 #include "ScrollingTreeScrollingNodeDelegateNicosia.h"
 #include "ThreadedScrollingTree.h"
 
@@ -69,17 +69,17 @@ bool ScrollingTreeFrameScrollingNodeNicosia::commitStateBeforeChildren(const Scr
     const auto& scrollingStateNode = downcast<ScrollingStateFrameScrollingNode>(stateNode);
 
     if (scrollingStateNode.hasChangedProperty(ScrollingStateNode::Property::RootContentsLayer))
-        m_rootContentsLayer = static_cast<Nicosia::CompositionLayer*>(scrollingStateNode.rootContentsLayer());
+        m_rootContentsLayer = static_cast<CoordinatedPlatformLayer*>(scrollingStateNode.rootContentsLayer());
     if (scrollingStateNode.hasChangedProperty(ScrollingStateNode::Property::CounterScrollingLayer))
-        m_counterScrollingLayer = static_cast<Nicosia::CompositionLayer*>(scrollingStateNode.counterScrollingLayer());
+        m_counterScrollingLayer = static_cast<CoordinatedPlatformLayer*>(scrollingStateNode.counterScrollingLayer());
     if (scrollingStateNode.hasChangedProperty(ScrollingStateNode::Property::InsetClipLayer))
-        m_insetClipLayer = static_cast<Nicosia::CompositionLayer*>(scrollingStateNode.insetClipLayer());
+        m_insetClipLayer = static_cast<CoordinatedPlatformLayer*>(scrollingStateNode.insetClipLayer());
     if (scrollingStateNode.hasChangedProperty(ScrollingStateNode::Property::ContentShadowLayer))
-        m_contentShadowLayer = static_cast<Nicosia::CompositionLayer*>(scrollingStateNode.contentShadowLayer());
+        m_contentShadowLayer = static_cast<CoordinatedPlatformLayer*>(scrollingStateNode.contentShadowLayer());
     if (scrollingStateNode.hasChangedProperty(ScrollingStateNode::Property::HeaderLayer))
-        m_headerLayer = static_cast<Nicosia::CompositionLayer*>(scrollingStateNode.headerLayer());
+        m_headerLayer = static_cast<CoordinatedPlatformLayer*>(scrollingStateNode.headerLayer());
     if (scrollingStateNode.hasChangedProperty(ScrollingStateNode::Property::FooterLayer))
-        m_footerLayer = static_cast<Nicosia::CompositionLayer*>(scrollingStateNode.footerLayer());
+        m_footerLayer = static_cast<CoordinatedPlatformLayer*>(scrollingStateNode.footerLayer());
 
     m_delegate->updateFromStateNode(scrollingStateNode);
     return true;
@@ -104,18 +104,19 @@ void ScrollingTreeFrameScrollingNodeNicosia::currentScrollPositionChanged(Scroll
 
 void ScrollingTreeFrameScrollingNodeNicosia::repositionScrollingLayers()
 {
-    auto* scrollLayer = static_cast<Nicosia::CompositionLayer*>(scrolledContentsLayer());
+    auto* scrollLayer = static_cast<CoordinatedPlatformLayer*>(scrolledContentsLayer());
     if (!scrollLayer)
         return;
 
-    auto scrollPosition = currentScrollPosition();
+    // If we're committing on the scrolling thread, it means that ThreadedScrollingTree is in "desynchronized" mode.
+    // The main thread may already have set the same layer position, but here we need to trigger a scrolling thread composition
+    // to ensure that the scroll happens even when the main thread commit is taking a long time. So make sure the layer property
+    // changes when there has been a scroll position change.
+    CoordinatedPlatformLayer::ForcePositionSync forceSync = ScrollingThread::isCurrentThread() && !scrollingTree()->isScrollingSynchronizedWithMainThread() ?
+        CoordinatedPlatformLayer::ForcePositionSync::Yes : CoordinatedPlatformLayer::ForcePositionSync::No;
 
-    scrollLayer->accessPending(
-        [&scrollPosition](Nicosia::CompositionLayer::LayerState& state)
-        {
-            state.position = -scrollPosition;
-            state.delta.positionChanged = true;
-        });
+    auto scrollPosition = currentScrollPosition();
+    scrollLayer->setPositionForScrolling(-scrollPosition, forceSync);
 }
 
 void ScrollingTreeFrameScrollingNodeNicosia::repositionRelatedLayers()
@@ -123,35 +124,21 @@ void ScrollingTreeFrameScrollingNodeNicosia::repositionRelatedLayers()
     auto scrollPosition = currentScrollPosition();
     auto layoutViewport = this->layoutViewport();
 
-    FloatRect visibleContentRect(scrollPosition, scrollableAreaSize());
-
-    auto applyLayerPosition =
-        [](auto& layer, auto&& position)
-        {
-            layer.accessPending(
-                [&position](Nicosia::CompositionLayer::LayerState& state)
-                {
-                    state.position = position;
-                    state.delta.positionChanged = true;
-                });
-        };
-
     if (m_counterScrollingLayer)
-        applyLayerPosition(*m_counterScrollingLayer, layoutViewport.location());
+        m_counterScrollingLayer->setPositionForScrolling(layoutViewport.location());
 
     float topContentInset = this->topContentInset();
     if (m_insetClipLayer && m_rootContentsLayer) {
-        m_insetClipLayer->accessPending(
-            [&scrollPosition, &topContentInset](Nicosia::CompositionLayer::LayerState& state)
-            {
-                state.position = { state.position.x(), LocalFrameView::yPositionForInsetClipLayer(scrollPosition, topContentInset) };
-                state.delta.positionChanged = true;
-            });
-
+        FloatPoint insetClipPosition;
+        {
+            Locker locker { m_insetClipLayer->lock() };
+            insetClipPosition = FloatPoint(m_insetClipLayer->position().x(), LocalFrameView::yPositionForInsetClipLayer(scrollPosition, topContentInset));
+        }
+        m_insetClipLayer->setPositionForScrolling(insetClipPosition);
         auto rootContentsPosition = LocalFrameView::positionForRootContentLayer(scrollPosition, scrollOrigin(), topContentInset, headerHeight());
-        applyLayerPosition(*m_rootContentsLayer, rootContentsPosition);
+        m_rootContentsLayer->setPositionForScrolling(rootContentsPosition);
         if (m_contentShadowLayer)
-            applyLayerPosition(*m_contentShadowLayer, rootContentsPosition);
+            m_contentShadowLayer->setPositionForScrolling(rootContentsPosition);
     }
 
     if (m_headerLayer || m_footerLayer) {
@@ -160,9 +147,9 @@ void ScrollingTreeFrameScrollingNodeNicosia::repositionRelatedLayers()
         // then we should recompute layoutViewport.x() for the banner with a scale factor of 1.
         float horizontalScrollOffsetForBanner = layoutViewport.x();
         if (m_headerLayer)
-            applyLayerPosition(*m_headerLayer, FloatPoint(horizontalScrollOffsetForBanner, LocalFrameView::yPositionForHeaderLayer(scrollPosition, topContentInset)));
+            m_headerLayer->setPositionForScrolling(FloatPoint(horizontalScrollOffsetForBanner, LocalFrameView::yPositionForHeaderLayer(scrollPosition, topContentInset)));
         if (m_footerLayer)
-            applyLayerPosition(*m_footerLayer, FloatPoint(horizontalScrollOffsetForBanner, LocalFrameView::yPositionForFooterLayer(scrollPosition, topContentInset, totalContentsSize().height(), footerHeight())));
+            m_footerLayer->setPositionForScrolling(FloatPoint(horizontalScrollOffsetForBanner, LocalFrameView::yPositionForFooterLayer(scrollPosition, topContentInset, totalContentsSize().height(), footerHeight())));
     }
 
     delegate().updateVisibleLengths();

commit 9ebe9d89eaef08c00b6fb8749d7fb715b7434cd5

REGRESSION(287835@main): [CoordinatedGraphics] Scrolling by clicking on any scrollbar position is broken https://bugs.webkit.org/show_bug.cgi?id=284877
Reviewed by Alejandro G. Castro.

This broke in 287835@main because the CoordinatedPlatformLayer is not
updated when GraphicsLayerCoordinated::syncPosition is called. This is
the expected behavior, the position will be set on
CoordinatedPlatformLayer later when scrolling layers are positioned. The
problem is that GraphicsLayerCoordinated is using the
CoordinatedPlatformLayer properties to compute the layer transform, and
now it's using the previous position, causing a wrong transformed
visible rect to be set in the CoordinatedPlatformLayer. We should use
the GraphicsLayer properties when computing the transforms.

* Source/WebCore/platform/graphics/texmap/coordinated/GraphicsLayerCoordinated.cpp:
(WebCore::GraphicsLayerCoordinated::computeLayerTransformIfNeeded):

Canonical link: https://commits.webkit.org/288079@main

Source/WebCore/platform/graphics/texmap/coordinated/GraphicsLayerCoordinated.cpp
@@ -771,12 +771,12 @@ void GraphicsLayerCoordinated::computeLayerTransformIfNeeded(bool affectedByTran
 
     m_layerTransform.current.setLocalTransform(currentTransform);
 
-    m_layerTransform.current.setAnchorPoint(m_platformLayer->anchorPoint());
-    m_layerTransform.current.setPosition(FloatPoint(m_platformLayer->position() - m_platformLayer->boundsOrigin()));
-    m_layerTransform.current.setSize(m_platformLayer->size());
+    m_layerTransform.current.setAnchorPoint(m_anchorPoint);
+    m_layerTransform.current.setPosition(FloatPoint(m_position - m_boundsOrigin));
+    m_layerTransform.current.setSize(m_size);
 
     m_layerTransform.current.setFlattening(!m_preserves3D);
-    m_layerTransform.current.setChildrenTransform(m_platformLayer->childrenTransform());
+    m_layerTransform.current.setChildrenTransform(childrenTransform());
     m_layerTransform.current.combineTransforms(parent() ? downcast<GraphicsLayerCoordinated>(*parent()).m_layerTransform.current.combinedForChildren() : TransformationMatrix());
 
     m_layerTransform.cachedCombined = m_layerTransform.current.combined();